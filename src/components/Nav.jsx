e2888a0ff3ff3189d62a202f6f7404a67115087c
Roshan panda <roshanpanda666@gmail.com>
2023-03-19 20:21:01 +0530

    commiting navbar updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css'
 import { Link } from 'react-router-dom'
 const Nav = () => {
+  let[beforhome,clickhome]=useState()
+  let[befordestination,clickdestination]=useState()
+  let[beforcrew,clickcrew]=useState()
+  let[befortech,clicktech]=useState()
+  const home=()=>{
+    clickhome("homeclick")
+  }
+  const destination=()=>{
+    clickdestination("homeclick")
+  }
+  const crew=()=>{
+    clickcrew("homeclick")
+  }
+  const tech=()=>{
+    clicktech("homeclick")
+  }
   return (
     <div className="parrentnav">
       <div className='fathernav'>
 
-<div className='home'>
-<Link to={"/"}>Home</Link>
-</div>
+        <div >
+        <Link className={beforhome}onClick={home} to={"/"}>Home</Link>
+        </div>
 
-<div className='destination'>
-<Link to={"/Destination"}>Destination</Link>
-</div>
+        <div >
+        <Link className={befordestination}onClick={destination} to={"/Destination"}>Destination</Link>
+        </div>
 
-<div className='crew'>
-<Link to={"/Crew"}>Crew</Link>
-</div>
+        <div >
+        <Link className={beforcrew}onClick={crew} to={"/Crew"}>Crew</Link>
+        </div>
 
-<div className='tech'>
-<Link to={"/Tech"}>Tech</Link>
-</div>
+        <div onClick={tech}>
+        <Link  className={befortech} to={"/Tech"}>Tech</Link>
+        </div>
 
-</div>
+      </div>
     </div>
     
   )
